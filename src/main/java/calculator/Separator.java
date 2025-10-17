398a603a6cd75c652fb2b8a9c088e8cee88d66e1
배재훈 <lange50300@gmail.com>
2025-10-17 20:23:18 +0900

    feat(Separator): Separator 클래스 생성
    - Separator 클래스를 통해 구분한 커스텀 구분자를 통해서 숫자 부분에서 숫자를 추출해내어 계산기에 넘김

--- src/main/java/calculator/Separator.java
+++ src/main/java/calculator/Separator.java
@@ -1,30 +1,37 @@
 package calculator;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 
 public class Separator {
+    String baseDelimiter = "[.,]";
+    ArrayList<Integer> result = new ArrayList<>();
+    InputValidate inputValidate = new InputValidate();
 
-    private static final String customSeparatorPrefix = "//";
-    private static final String customSeparatorSuffix = "\n";
-    ArrayList<String> separators = new ArrayList<>(Arrays.asList(",", ":"));
+    public ArrayList<Integer> separateNumberWithCustom(String input) {
+        inputValidate.validateInputWithStartCustom(input);
 
-    public void splitSeparatorAndNumber(UserInputDto userInputDto) {
-        String input = userInputDto.getInput();
-        if (input.startsWith(customSeparatorPrefix)) {
-            userInputDto.setInputSeparator(input.substring(0, 5));
-            userInputDto.setInputNumber(input.substring(5));
+        String numberInclude = input.substring(5);
+        String customDelimiter = input.substring(2, 3);
+
+        baseDelimiter = baseDelimiter.substring(0, baseDelimiter.length() - 1) + customDelimiter + "]";
+        String[] numbers = numberInclude.split(baseDelimiter);
+
+        for (String number : numbers) {
+            result.add(Integer.parseInt(number));
         }
 
-        if (!input.startsWith(customSeparatorPrefix)) {
-            userInputDto.setInputNumber(input);
-        }
-
+        return result;
     }
 
-    public ArrayList<String> getSeparators(String inputSeparator) {
-        separators.add(inputSeparator.substring(2, 3));
-        return separators;
+    public ArrayList<Integer> separateNumberNoCustom(String input) {
+        inputValidate.validateInputNotStartCustom(input);
+        String[] numbers = input.split(baseDelimiter);
+
+        for (String number : numbers) {
+            result.add(Integer.parseInt(number));
+        }
+
+        return result;
     }
 
 }
