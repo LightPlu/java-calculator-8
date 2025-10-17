7a0bd4b31777d5b1786fe6e4e9bc80a3bec73eb8
배재훈 <lange50300@gmail.com>
2025-10-17 21:17:21 +0900

    feat(main): Application 클래스 main 메서드 구현
    - 입출력 요구 사항에 맞춰 안내 메세지를 출력하고 문자열을 입력 받은 뒤 계산기 기능을 통해 출력값을 제공

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,18 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        CalculatorController CalculatorController = new CalculatorController();
+        UserInputDto userinputDto = new UserInputDto();
+        int result;
+
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        userinputDto.setInput(Console.readLine());
+        result = CalculatorController.CalculatorControl(userinputDto.getInput());
+
+        System.out.println("결과 : " + result);
     }
 }
