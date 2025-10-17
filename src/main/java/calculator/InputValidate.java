a77659b30eb6c369f2e54f0544fe294029eb3f44
배재훈 <lange50300@gmail.com>
2025-10-17 19:47:51 +0900

    feat(inputValidate): InputValidate 클래스 생성
    - 사용자로부터 입력받은 문자열 검증 로직 추가
    - 커스텀 구분자 형태 검증, 숫자 부분 타입 검증, 숫자 부분 구분자 개수 검증 총 3가지 검증 로직으로 구성

---
+++ src/main/java/calculator/InputValidate.java
@@ -0,0 +1,86 @@
+package calculator;
+
+public class InputValidate {
+
+    InputValidate(String input) {
+        validateInputWithStartCustom(input);
+        validateInputNotStartCustom(input);
+    }
+
+    public void validateInputWithStartCustom(String input) {
+        validateCustomForm(input);
+        validateTypeOfNumber(input, extractCustomDelimiter(input));
+        validateDelimiterCount(input, extractCustomDelimiter(input));
+    }
+
+    public void validateInputNotStartCustom(String input) {
+        validateTypeOfNumber(input, null);
+        validateDelimiterCount(input, null);
+    }
+
+    public String extractCustomDelimiter(String input) {
+        return input.substring(2, 3);
+    }
+
+    public void validateCustomForm(String input) {
+        if (input.length() < 5) {
+            throw new IllegalArgumentException();    // 커스텀 구분자를 표시하는 문자열이 5보다 작다면 에러 발생
+        }
+
+        String delimiterInclude = input.substring(0, 5);
+        String customDelimiter = input.substring(2, 3);
+
+        if (!delimiterInclude.startsWith("//")) {
+            throw new IllegalArgumentException();
+        }
+
+        if (!delimiterInclude.endsWith("\\n")) {
+            throw new IllegalArgumentException();
+        }
+
+        if (customDelimiter.matches("[0-9a-zA-Z]")) {    //숫자 혹은 문자가 온다면 에러 발생
+            throw new IllegalArgumentException();
+        }
+
+    }
+
+    public void validateTypeOfNumber(String input, String customDelimiter) {
+        String basedelimiter = "[.,]";
+        if (customDelimiter != null && !customDelimiter.isEmpty()) {
+            basedelimiter = basedelimiter.substring(0, basedelimiter.length() - 1) + customDelimiter + "]";
+        }
+        String[] numbers = input.split(basedelimiter);
+
+        if (numbers[0].isEmpty()) {
+            throw new IllegalArgumentException(); // 만약 숫자 부분 문자열이 숫자가 아닌 구분자로 바로 시작한다면 에러 발생(구분자로 시작하면 numbers 첫번째 값이 비어있기 때문)
+        }
+
+        try {
+            for (String number : numbers) {
+                Integer.parseInt(number);     // 구분자로 분리했을 때 해당 문자열이 숫자가 아니라면 에러 발생
+            }
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException();
+        }
+
+    }
+
+    public void validateDelimiterCount(String input, String customDelimiter) {
+        String basedelimiter = "[.,]";
+        if (customDelimiter != null && !customDelimiter.isEmpty()) {
+            basedelimiter = basedelimiter.substring(0, basedelimiter.length() - 1) + customDelimiter + "]";
+        }
+        String[] numbers = input.split(basedelimiter);
+        long commaCount = input.chars().filter(ch -> ch == ',').count();
+        long dotCount = input.chars().filter(ch -> ch == '.').count();
+
+        if (numbers[0].isEmpty()) {
+            throw new IllegalArgumentException(); // 만약 숫자 부분 문자열이 숫자가 아닌 구분자로 바로 시작한다면 에러 발생(구분자로 시작하면 numbers 첫번째 값이 비어있기 때문)
+        }
+
+        if (numbers.length + 1 != (dotCount + commaCount)) {        // 구분자가 동시에 2개이상 왔을 때 에러 발생
+            throw new IllegalArgumentException();
+        }
+    }
+
+}
